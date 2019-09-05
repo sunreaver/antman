46deca727f08c88c0736b064e40e825b999879cf
TheOne <tanwei.rush@gmail.com>
2019-09-05 10:36:28 +0800

    1. redis config with timesize 2. 3. 4.

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/sunreaver/antman
+
+go 1.12
+
+require (
+	github.com/Shopify/sarama v1.23.1
+	github.com/go-redis/redis v6.15.5+incompatible
+	github.com/jinzhu/gorm v1.9.10
+	github.com/sunreaver/logger v1.0.0
+	github.com/sunreaver/tomlanalysis v1.0.0
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+)
--- redis/config.go
+++ redis/config.go
@@ -1,12 +1,16 @@
 package redis
 
+import (
+	"github.com/sunreaver/tomlanalysis/timesize"
+)
+
 // Config Config
 type Config struct {
-	Hosts        []string `toml:"hosts"`
-	Prefix       string   `toml:"prefix"`
-	DialTimeout  int      `toml:"dial_timeout"`
-	WriteTimeout int      `toml:"write_timeout"`
-	ReadTimeout  int      `toml:"read_timeout"`
-	Poolsize     int      `toml:"poolsize"`
-	PoolTimeout  int      `toml:"pool_timeout"`
+	Hosts        []string          `toml:"hosts"`
+	Prefix       string            `toml:"prefix"`
+	DialTimeout  timesize.Duration `toml:"dial_timeout"`
+	WriteTimeout timesize.Duration `toml:"write_timeout"`
+	ReadTimeout  timesize.Duration `toml:"read_timeout"`
+	Poolsize     int               `toml:"poolsize"`
+	PoolTimeout  int               `toml:"pool_timeout"`
 }
